eb506c9ce25e9e2779dc1bbc533e763d2ad96fbe
Nikolay Abadeev <voood234@gmail.com>
2021-12-12 23:56:54 +0300

    Update

--- src/main/java/ru/netology/sqr/SqrService.java
+++ src/main/java/ru/netology/sqr/SqrService.java
@@ -15,13 +15,3 @@ public class SqrService {
 
 
 
-    //public long calculate(long amount, boolean registered) {
-    //int percent = registered ? 3 : 1;
-       // long bonus = amount * percent/100/100;
-      //  long limit =500;
-      //  if (bonus > limit) {
-          //  bonus = limit;
-     //   }
-      //  return bonus;
-   // }
-//}
